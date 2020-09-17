ed7bb0f6e715aca3eabe63d5f6b2707cefbe5e2b
harulightbule <peche.saison.1023@gmail.com>
2020-09-17 17:56:39 +0900

    itemsコントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,17 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index]
 
   def index
+    @items = Item.all # controller <= Item model => DB
+    @items = Item.order("created_at DESC")
   end
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
+  
+  def new
+    @items = Item.new
+  end
+
+
 end
