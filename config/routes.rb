0fa3017a4de29bd25c24dce03ff511f0ed086217
harulightbule <peche.saison.1023@gmail.com>
2020-09-28 21:15:26 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:index, :new, :create, :show]
   resources :users, only: :show
+  resources :items, only: [:index, :new, :create, :show] do
+    resources :orders, only: [:index, :new, :create]
+  end
 end
