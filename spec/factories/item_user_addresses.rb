c982cd2880ad0a0b02bea5dccd8a1ef9e1567543
harulightbule <peche.saison.1023@gmail.com>
2020-09-28 22:50:39 +0900

    モデル単体テストコード

---
+++ spec/factories/item_user_addresses.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :item_user_address do
+    postal_code { '123-0052'}
+    prefecture { 2 }
+    city       { '名古屋市' }
+    house_number {'1-1'}
+    building_name {'朝日マンション'}
+    phone_number { '09012345678' }
+    token        { '00000000000' }
+    
+  end
+end
