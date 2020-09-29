c348c42e647f0c5be73461796bf70655e48c5b42
harulightbule <peche.saison.1023@gmail.com>
2020-09-29 17:22:39 +0900

    Revert "ルーティングの設定"
    This reverts commit 3d636f1ad3c42a89dc8d1d9302e319a45c25b261.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :users, only: :show
-  resources :items, only: [:index, :new, :create, :show, :edit, :update] do
+  resources :items, only: [:index, :new, :create, :show] do
     resources :orders, only: [:index, :new, :create]
   end
 end
