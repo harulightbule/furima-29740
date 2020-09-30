481682a35079165d58fb474f3c8a96229d3fa262
harulightbule <peche.saison.1023@gmail.com>
2020-09-30 11:11:49 +0900

    アソシエーションの編集

--- app/models/item_user.rb
+++ app/models/item_user.rb
@@ -1,5 +1,6 @@
 class ItemUser < ApplicationRecord
   belongs_to :item
   belongs_to :user
+  belongs_to :item
   has_one :address
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  has_many :items
-  has_many :items_users
+  has_many :items, dependent: :destroy
+  has_many :item_users, dependent: :destroy
+  has_many :items, through: :item_users
   with_options presence: true do
     validates :nickname
     validates :password, format: { with: /[a-z\d]{6,}/i }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
